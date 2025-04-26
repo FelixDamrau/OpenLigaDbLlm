a7f1b5ec55499c053d69523a2951a0f7ae3bfcd2
Felix Damrau <mail@felixdamrau.de>
2025-04-26 21:06:48 +0200

    Add getAllTeams tool, use cancellation token in all tools

--- src/McpServer/Tools/OpenLigaDbTools.cs
+++ src/McpServer/Tools/OpenLigaDbTools.cs
@@ -11,21 +11,35 @@ public class OpenLigaDbTools(OpenLigaDbServiceClient client, ILogger<OpenLigaDbT
     private readonly ILogger<OpenLigaDbTools> logger = logger;
 
     [McpServerTool(Name = "getAvailableLeagues"), Description("Gets all available leagues")]
-    public async Task<ICollection<League>> GetAvailableLeagues()
+    public async Task<ICollection<League>> GetAvailableLeagues(CancellationToken cancellationToken)
     {
         logger.Log(LogLevel.Debug, "[MCP Server Tool] Getting all leagues");
-        return await client.GetavailableleaguesAsync().ConfigureAwait(false);
+        return await client.GetavailableleaguesAsync(cancellationToken).ConfigureAwait(false);
     }
 
     [McpServerTool(Name = "getFilteredLeagues"), Description("Gets all available leagues that match the given filter")]
-    public async Task<ICollection<League>> GetAvailableLeagues(string filter)
+    public async Task<ICollection<League>> GetAvailableLeagues(
+        [Description("The filter that is used to filter the league name. (Contains, case-insensitve)")] string filter,
+        CancellationToken cancellationToken)
     {
         logger.Log(LogLevel.Debug, "[MCP Server Tool] Filtering leagues with filter: {Filter}", filter);
         var leagues = await client
-            .GetavailableleaguesAsync()
+            .GetavailableleaguesAsync(cancellationToken)
             .ConfigureAwait(false);
 
         return [.. leagues.Where(x => x.LeagueName.Contains(filter, StringComparison.OrdinalIgnoreCase))];
+    }
+
+    [McpServerTool(Name = "getAllTeams"), Description("Gets all teams for the given league in the given season")]
+    public async Task<ICollection<Team>> GetAllTeams(
+        [Description("The league shortcut, like bl1")] string leagueShortcut,
+        [Description("The year of the season, e.g. 2024 for the season 2024/25")] int leagueSeason,
+        CancellationToken cancellationToken)
+    {
+        logger.Log(LogLevel.Debug, "[MCP Server Tool] Getting all teams for league/season: {LeagueShortcut}/{LeagueSeason}", leagueShortcut, leagueSeason);
+        return await client
+            .GetavailableteamsAsync(leagueShortcut, leagueSeason, cancellationToken)
+            .ConfigureAwait(false);
     }
 
     [McpServerTool(Name = "echo"), Description("Echoes the given message.")]
