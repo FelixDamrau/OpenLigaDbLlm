d4f64ff2f5906812a93402181039dee1f87f4d05
Felix Damrau <mail@felixdamrau.de>
2025-04-29 18:32:10 +0200

    Add 'getMatches' tools

--- src/McpServer/Tools/OpenLigaDbTools.cs
+++ src/McpServer/Tools/OpenLigaDbTools.cs
@@ -51,6 +51,42 @@ public class OpenLigaDbTools(OpenLigaDbServiceClient client, ILogger<OpenLigaDbT
             .ConfigureAwait(false);
     }
 
+    [McpServerTool(Name = "getMatchesByLeagueAndTeam"), Description("Gets all teams for the given league in the given season")]
+    public async Task<ICollection<Match>> GetMatches(
+        [Description("The league shortcut, like bl1")] string leagueShortcut,
+        [Description("The year of the season, e.g. 2024 for the season 2024/25")] int leagueSeason,
+        [Description("A filter string for the team in question (case sensitive), e.g. Bayern for FC Bayern München.")] string teamFilter,
+        CancellationToken cancellationToken)
+    {
+        using (Serilog.Context.LogContext.PushProperty("tool.name", "getMatchesByLeagueAndTeam"))
+        {
+            logger.Log(
+                LogLevel.Debug,
+                "[MCP Server Tool] Getting all teams for league/season/team filter: {LeagueShortcut}/{LeagueSeason}/{teamFilter}", leagueShortcut, leagueSeason, teamFilter);
+        }
+        return await client
+            .GetmatchdataAll3Async(leagueShortcut, leagueSeason, teamFilter, cancellationToken)
+            .ConfigureAwait(false);
+    }
+
+    [McpServerTool(Name = "getMatchesByLeagueAndDay"), Description("Gets all matches of the given day of a given league.")]
+    public async Task<ICollection<Match>> GetMatches(
+        [Description("The league shortcut, like bl1")] string leagueShortcut,
+        [Description("The year of the season, e.g. 2024 for the season 2024/25")] int leagueSeason,
+        [Description("The match day of the league")] int matchDay,
+        CancellationToken cancellationToken)
+    {
+        using (Serilog.Context.LogContext.PushProperty("tool.name", "getMatchesByLeagueAndDay"))
+        {
+            logger.Log(
+                LogLevel.Debug,
+                "[MCP Server Tool] Getting all teams for league/season/match day: {LeagueShortcut}/{LeagueSeason}/{matchDay}", leagueShortcut, leagueSeason, matchDay);
+        }
+        return await client
+            .GetmatchdataAllAsync(leagueShortcut, leagueSeason, matchDay, cancellationToken)
+            .ConfigureAwait(false);
+    }
+
     [McpServerTool(Name = "echo"), Description("Echoes the given message.")]
     public string Echo(string message)
     {
